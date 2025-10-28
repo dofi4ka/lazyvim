7fadc2a0e2f345cff942258fa2276a1282233211
Vladislav <121549716+dofi4ka@users.noreply.github.com>
2025-10-29 01:19:16 +0300

    fix: add comas

--- lua/plugins/devtools.lua
+++ lua/plugins/devtools.lua
@@ -12,8 +12,8 @@ return {
         "stevearc/conform.nvim",
         opts = {
             formatters_by_ft = {
-                python = { "ruff" }
-                c = { "clang_format" }
+                python = { "ruff" },
+                c = { "clang_format" },
             },
             format_on_save = {
                 timeout_ms = 500,
