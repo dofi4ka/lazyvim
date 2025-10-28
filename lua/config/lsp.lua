5718b50e6370dfa00c89e00eb8a07fe1f8e6dc8a
Vladislav <121549716+dofi4ka@users.noreply.github.com>
2025-10-28 21:21:19 +0300

    Update lsp.lua

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,3 @@
 local lspconfig = require("lspconfig")
 
-local servers = { "clangd", "nil_ls", "pyright", "lua_ls" }
-
-for _, lsp in ipairs(servers) do
-  if vim.fn.executable(lsp) == 1 then
-    lspconfig[lsp].setup{}
-  end
-end
+lspconfig.clangd.setup{}
