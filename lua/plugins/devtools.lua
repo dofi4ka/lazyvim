01e717052276ed2650d0dde71616089af5a7992c
Vladislav <121549716+dofi4ka@users.noreply.github.com>
2025-11-01 11:29:03 +0300

    Update devtools.lua
    update gleam treesitter

--- lua/plugins/devtools.lua
+++ lua/plugins/devtools.lua
@@ -1,5 +1,9 @@
 return {
     {
+        "nvim-treesitter/nvim-treesitter",
+        opts = { ensure_installed = { "gleam" } },
+    },
+    {
         "neovim/nvim-lspconfig",
         opts = {
             servers = {
